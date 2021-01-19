cee1f117e6d93d677c3cda48a6df4f912d67ae0f
Nilay Savant <nilayofficial@gmail.com>
2021-01-19 18:51:04 +0530

    Remove manual hello api and refactor/prettify code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 mod models;
-use models::server_error::{ServerError, map_to_server_error};
+use models::server_error::{map_to_server_error, ServerError};
 
 #[get("/")]
 async fn hello() -> impl Responder {
@@ -9,23 +9,15 @@ async fn hello() -> impl Responder {
 
 #[post("/echo")]
 async fn echo(req_body: String) -> Result<HttpResponse, ServerError> {
-    let json_body: serde_json::Value = serde_json::from_str(req_body.as_str()).map_err(map_to_server_error)?;
+    let json_body: serde_json::Value =
+        serde_json::from_str(req_body.as_str()).map_err(map_to_server_error)?;
     Ok(HttpResponse::Ok().json(json_body))
-}
-
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(hello).service(echo))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
