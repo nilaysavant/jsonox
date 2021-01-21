bc57f9f1d724cdb9980ca297ddd9eb80d868bf7d
Nilay Savant <nilayofficial@gmail.com>
2021-01-22 02:27:18 +0530

    Update App description

--- src/constants/app_metadata.rs
+++ src/constants/app_metadata.rs
@@ -1,4 +1,4 @@
 pub const APP_NAME: &str = "jsonox";
-pub const APP_DESCRIPTION: &str = "RESTful JSON server + store written in Rust";
+pub const APP_DESCRIPTION: &str = "CLI based RESTful JSON server + store written in Rust.";
 pub const APP_VERSION: &str = "0.1.0";
 pub const APP_AUTHOR: &str = "Nilay Savant <nilayofficial@gmail.com>";
