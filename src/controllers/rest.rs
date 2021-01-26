01bcc53ab8adc9588bb53b2066e4a73ebb56e4c5
Nilay Savant <nilayofficial@gmail.com>
2021-01-27 05:07:47 +0530

    Create: read_only_handler to handle all request that are in read-only mode, impl this in post_json_to_path handler WIP

--- src/controllers/rest.rs
+++ src/controllers/rest.rs
@@ -2,13 +2,16 @@ use std::path::PathBuf;
 
 use crate::{
     constants::paths::APP_DATA_DIR,
-    models::server_error::{map_to_server_error, ServerError},
+    models::{
+        app_state::{AppMode, AppState},
+        server_error::{map_to_server_error, ServerError},
+    },
     utils::fsutils::{read_from_path, remove_from_path, write_to_path},
 };
 use actix_web::{
     delete, get, route,
     web::{self},
-    HttpResponse,
+    HttpRequest, HttpResponse,
 };
 use relative_path::RelativePath;
 use serde_json::json;
@@ -44,28 +47,64 @@ pub async fn list_active_paths() -> Result<HttpResponse, ServerError> {
     Ok(HttpResponse::Ok().json(json!({ "active_paths": active_paths })))
 }
 
+/// Request Handler incase of `ReadOnly` Mode
+/// * `file_name` - Name of the `json` file to read and send as response
+pub fn read_only_handler(
+    url_path: web::Path<String>,
+    file_name: &str,
+) -> Result<HttpResponse, ServerError> {
+    let file_path = RelativePath::new(url_path.as_str())
+        .join(file_name)
+        .normalize()
+        .to_path(APP_DATA_DIR)
+        .to_owned();
+    if file_path.starts_with(APP_DATA_DIR) {
+        let json_string = read_from_path(file_path.as_path())?;
+        let json_data: serde_json::Value =
+            serde_json::from_str(&json_string).map_err(map_to_server_error)?;
+        Ok(HttpResponse::Ok().json(json_data))
+    } else {
+        Err(ServerError::UserError {
+            message: "JSON file path invalid!".to_string(),
+        })
+    }
+}
+
 /// Post/Put JSON to specified path
 /// - This creates a json file in the project data dir(in the specified path) with the posted json data
 #[route("/{url_path:.*}", method = "POST", method = "PUT")] // Allow POST + PUT handling
 pub async fn post_json_to_path(
+    req: HttpRequest,
     url_path: web::Path<String>,
     req_body: String,
+    data: web::Data<AppState>,
 ) -> Result<HttpResponse, ServerError> {
     if url_path.len() > 0 {
-        let file_path = RelativePath::new(url_path.as_str())
-            .join("index.json")
-            .normalize()
-            .to_path(APP_DATA_DIR)
-            .to_owned();
-        if file_path.starts_with(APP_DATA_DIR) {
-            let body: serde_json::Value =
-                serde_json::from_str(&req_body).map_err(map_to_server_error)?;
-            write_to_path(file_path.as_path(), &body)?;
-            Ok(HttpResponse::Ok().json(body))
-        } else {
-            Err(ServerError::UserError {
-                message: "JSON file path invalid!".to_string(),
-            })
+        match &data.mode {
+            AppMode::Normal => {
+                let file_path = RelativePath::new(url_path.as_str())
+                    .join("index.json")
+                    .normalize()
+                    .to_path(APP_DATA_DIR)
+                    .to_owned();
+                if file_path.starts_with(APP_DATA_DIR) {
+                    let body: serde_json::Value =
+                        serde_json::from_str(&req_body).map_err(map_to_server_error)?;
+                    write_to_path(file_path.as_path(), &body)?;
+                    Ok(HttpResponse::Ok().json(body))
+                } else {
+                    Err(ServerError::UserError {
+                        message: "JSON file path invalid!".to_string(),
+                    })
+                }
+            }
+            AppMode::ReadOnly => {
+                if req.method() == "PUT" {
+                    read_only_handler(url_path, "put.json")
+                } else {
+                    read_only_handler(url_path, "post.json")
+                }
+            }
         }
     } else {
         Err(ServerError::UserError {
